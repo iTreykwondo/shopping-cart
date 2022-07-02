f42af4af6960af49723a42a6205b75f97484077c
Trajan Miller <trajanmiller324@gmail.com>
2022-07-02 19:55:49 -0400

    Load all products from api

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,11 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import Navbar from "./Navbar";
 
 const Shop = () => {
+  const [products, setProducts] = useState([]);
+
+  const loadProducts = async () => {
+    let response = await fetch("https://fakestoreapi.com/products");
+    let data = await response.json();
+
+    setProducts(data);
+
+    return data;
+  };
+
+  useEffect(() => {
+    loadProducts();
+    console.log(products);
+  }, []);
+
   return (
     <div>
       <Navbar />
+      <h1>Shop Page</h1>
     </div>
   );
 };
