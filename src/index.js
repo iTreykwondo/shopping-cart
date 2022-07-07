09d12ef60976369bd83d676c9a4f469f1d6dc8ec
Trajan Miller <trajanmiller324@gmail.com>
2022-07-06 21:35:55 -0400

    Import Browser Router

--- src/index.js
+++ src/index.js
@@ -2,17 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Shop from "./components/Shop";
-import Cart from "./components/Cart";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="/shop" element={<Shop />} />
-      <Route path="/cart" element={<Cart />} />
-    </Routes>
+    <App />
   </BrowserRouter>
 );
