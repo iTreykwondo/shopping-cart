712f3b5839735962c704f31c7c1ca777c4ce5866
Trajan Miller <trajanmiller324@gmail.com>
2022-07-06 21:32:18 -0400

    Render cart items from props

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,9 +1,26 @@
-import Navbar from "./Navbar";
-
-const Cart = () => {
+const Cart = (props) => {
   return (
     <div>
-      <Navbar />
+      <h1>Cart</h1>
+      {props.cart.map((item, index) => {
+        return (
+          <div className="container" key={index}>
+            <h2>{item.itemName}</h2>
+            <p>{item.itemPrice}</p>
+          </div>
+        );
+      })}
+      <div className="order-container">
+        <div className="total">
+          <h3>Order Total: {props.total}</h3>
+        </div>
+        <div className="btn-container">
+          <button className="clear-btn">Clear cart</button>
+          <button className="checkout-btn" onClick={props.checkout}>
+            Checkout
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
